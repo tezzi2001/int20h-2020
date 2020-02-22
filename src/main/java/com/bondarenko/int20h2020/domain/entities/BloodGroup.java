1f492e83e195914f3bf491a34f0f36eb273b70f5
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-22 15:44:53 +0200

    refactor: add methods to access and modify fields in BloodGroup type

--- src/main/java/com/bondarenko/int20h2020/domain/entities/BloodGroup.java
+++ src/main/java/com/bondarenko/int20h2020/domain/entities/BloodGroup.java
@@ -1,8 +1,15 @@
 package com.bondarenko.int20h2020.domain.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BloodGroup {
     private int groupNumber;
     private char rh;
