bee6fb4d07333c1e2f7f4152dceccea7e9911f9d
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-22 13:16:45 +0200

    feat: add profiles for develop and production servers

--- src/main/java/com/bondarenko/int20h2020/Int20h2020Application.java
+++ src/main/java/com/bondarenko/int20h2020/Int20h2020Application.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Int20h2020Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Int20h2020Application.class, args);
+        String dbUrl = System.getenv("DATABASE_URL");
+        if (dbUrl == null) {
+            System.err.println("Env var 'DATABASE_URL' is not defined!\n" +
+                    "Its value should be equals to 'dev' on develop server");
+            System.exit(0);
+        }
+        if ("DEV".equals(dbUrl)) {
+            SpringApplication.run(Int20h2020Application.class, args).getEnvironment().setActiveProfiles("dev");
+        } else {
+            SpringApplication.run(Int20h2020Application.class, args).getEnvironment().setActiveProfiles("prod");
+        }
     }
-
 }
