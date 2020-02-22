00d03b4e1549625e44609d57c33da022b3e96823
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-22 16:09:39 +0200

    feat: add Session entity

---
+++ src/main/java/com/bondarenko/int20h2020/domain/entities/Session.java
@@ -0,0 +1,23 @@
+package com.bondarenko.int20h2020.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Session {
+    @Id
+    private String login;
+    private String refreshToken;
+    private String fingerprint;
+    private Date expiresAt;
+    private Date createdAt;
+    private Date updatedAt;
+}
