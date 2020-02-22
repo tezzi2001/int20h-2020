473d993f66a8623cdd3d76c98c6c80dbb694ec53
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-22 19:32:15 +0200

    feat: add sign service

---
+++ src/main/java/com/bondarenko/int20h2020/services/SignService.java
@@ -0,0 +1,34 @@
+package com.bondarenko.int20h2020.services;
+
+import com.bondarenko.int20h2020.domain.JWT;
+import com.bondarenko.int20h2020.domain.entities.Person;
+import com.bondarenko.int20h2020.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SignService implements ISignService {
+    private UserRepository userRepository;
+    private IJwtService jwtService;
+
+    @Override
+    public JWT register(Person user) {
+        return null;
+    }
+
+    @Override
+    public JWT authenticate(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public JWT refresh(String refreshToken, String fingerprint) {
+        return null;
+    }
+
+    @Override
+    public boolean checkEmail(String email) {
+        return false;
+    }
+}
