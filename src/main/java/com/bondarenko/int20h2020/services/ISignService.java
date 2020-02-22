8b2284503e7287d7df32a0795cf57ca879a911d7
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-22 19:24:31 +0200

    refactor(ISignService): add fingerprint parameter for 'refresh' method

--- src/main/java/com/bondarenko/int20h2020/services/ISignService.java
+++ src/main/java/com/bondarenko/int20h2020/services/ISignService.java
@@ -6,6 +6,6 @@ import com.bondarenko.int20h2020.domain.entities.Person;
 public interface ISignService {
     public JWT register(Person user);
     public JWT authenticate(String email, String password);
-    public JWT refresh(String refreshToken);
+    public JWT refresh(String refreshToken, String fingerprint);
     public boolean checkEmail(String email);
 }
