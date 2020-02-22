5e150cd627546830a1f8e580902a7bc6767a8763
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-22 10:52:47 +0200

    init

---
+++ src/main/java/com/bondarenko/int20h2020/Int20h2020Application.java
@@ -0,0 +1,13 @@
+package com.bondarenko.int20h2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int20h2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Int20h2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bondarenko/int20h2020/Int20h2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bondarenko.int20h2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int20h2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
