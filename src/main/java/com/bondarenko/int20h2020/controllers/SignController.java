3eb0d143951435e9ff4dcf8705de58ac13b93bab
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-22 18:24:57 +0200

    feat(SignController): add mapping for path '/checkEmail'

--- src/main/java/com/bondarenko/int20h2020/controllers/SignController.java
+++ src/main/java/com/bondarenko/int20h2020/controllers/SignController.java
@@ -2,10 +2,21 @@ package com.bondarenko.int20h2020.controllers;
 
 import com.bondarenko.int20h2020.services.ISignService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 public class SignController {
     private ISignService signService;
+
+    @PostMapping("/checkEmail")
+    public Map<String, Boolean> checkEmail(String email) {
+        return new HashMap<String, Boolean>() {{
+            put("isExist", signService.checkEmail(email));
+        }};
+    }
 }
