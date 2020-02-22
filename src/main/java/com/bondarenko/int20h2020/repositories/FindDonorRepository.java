6af6874a4bdd46f291ad3295c42875b1d3b52cf2
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-23 00:20:49 +0200

    feat: add repositories for find- tables

---
+++ src/main/java/com/bondarenko/int20h2020/repositories/FindDonorRepository.java
@@ -0,0 +1,7 @@
+package com.bondarenko.int20h2020.repositories;
+
+import com.bondarenko.int20h2020.domain.entities.FindDonor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FindDonorRepository extends JpaRepository<FindDonor, Integer> {
+}
