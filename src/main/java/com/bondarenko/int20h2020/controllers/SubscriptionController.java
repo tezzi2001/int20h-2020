4f05691b6337b15eebc34cc0d07fa6456f5a9848
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-23 05:17:28 +0200

    feat: add subscription controller

---
+++ src/main/java/com/bondarenko/int20h2020/controllers/SubscriptionController.java
@@ -0,0 +1,31 @@
+package com.bondarenko.int20h2020.controllers;
+
+import com.bondarenko.int20h2020.services.ISubscriptionsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+public class SubscriptionController {
+    private ISubscriptionsService subscriptionsService;
+
+    @GetMapping("/getFollowers/{email}")
+    public List<String> getFollowers(@PathVariable String email) {
+        return subscriptionsService.getFollowers(email);
+    }
+
+    @PostMapping("/follow")
+    public void follow(String followerEmail, String email) {
+        subscriptionsService.follow(followerEmail, email);
+    }
+
+    @PostMapping("/unFollow")
+    public void unFollow(String followerEmail, String email) {
+        subscriptionsService.unFollow(followerEmail, email);
+    }
+}
